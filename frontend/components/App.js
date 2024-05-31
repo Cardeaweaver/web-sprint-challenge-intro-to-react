8ce739208757bc938c06800dea8f1e8c071ed186
carde'a weaver <165233514+Cardeaweaver@users.noreply.github.com>
2024-05-31 19:24:35 -0400

    Challenge 6

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -5,17 +5,27 @@ import Character from './Character'
 const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
-function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
+useEffect(()=> {
+  async function getData() {
+    const [resPeople, resPlanets] = await Promise.all([axios.get(urlPeople), axios.get(urlPlanets)])
+    let characters = resPeople.data.map(char => {
+      return {...char, homeworld: resPlanets.data.find(planet => planet.id == char.homeworld)}
+    })
+    setData(characters)
+  }
+  getData()
+}, [])
+  
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {data.map(char=> <Character key={char.id}/>)
+      /* ❗ Map over the data in state, rendering a Character at each iteration */
+      }
     </div>
   )
-}
+
 
 export default App
 
